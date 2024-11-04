34b3df38bf6b237811871d100404659b4f7fd04c
Mark Pinnock <mark.pinnock.18@ucl.ac.uk>
2024-11-04 19:21:51 +0000

    Add 2D operator unit tests

--- tests/common/test_operators.py
+++ tests/common/test_operators.py
@@ -7,7 +7,7 @@ import numpy as np
 import numpy.typing as npt
 import pytest
 
-from common.operators import custom_operator_1d, identity_operator
+from common.operators import custom_operator_1d, custom_operator_2d, identity_operator
 
 
 def test_identity_operator() -> None:
@@ -71,4 +71,35 @@ def test_custom_operator_1d_odd(
     kernel = np.array([1, -2, 1])
 
     operator = custom_operator_1d(kernel=kernel, arr_size=arr_size, conv_mode=conv_mode)
+    assert np.equal(operator.toarray(), expected_operator).all()
+
+
+@pytest.mark.parametrize(
+    ("conv_mode", "img_size", "expected_operator"),
+    [
+        ("valid", 4, "valid_4_first_order_2d"),
+        ("valid", 5, "valid_5_first_order_2d"),
+        ("same", 4, "same_4_first_order_2d"),
+        ("same", 5, "same_5_first_order_2d"),
+        ("periodic", 4, "periodic_4_first_order_2d"),
+        ("periodic", 5, "periodic_5_first_order_2d"),
+        ("full", 4, "full_4_first_order_2d"),
+        ("full", 5, "full_5_first_order_2d"),
+    ],
+)
+def test_custom_operator_2d_even(
+    conv_mode: str,
+    img_size: int,
+    expected_operator: npt.NDArray,
+    request: Any,
+) -> None:
+    """Test custom 2D operator for odd kernels."""
+    expected_operator = request.getfixturevalue(expected_operator)
+    kernel = np.array([[-1, 1], [-1, 1]])
+
+    operator = custom_operator_2d(
+        kernel=kernel,
+        image_size=img_size,
+        conv_mode=conv_mode,
+    )
     assert np.equal(operator.toarray(), expected_operator).all()
