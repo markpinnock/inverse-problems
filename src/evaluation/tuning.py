4115ead787e9327ddf70b6e036d3042a35b60c76
Mark Pinnock <mark.pinnock.18@ucl.ac.uk>
2024-11-12 08:48:08 +0000

    Add iterative tuner

--- src/evaluation/tuning.py
+++ src/evaluation/tuning.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from collections.abc import Callable
+from typing import Any
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -12,6 +13,7 @@ from skimage.metrics import (
     structural_similarity,
 )
 
+from common.operators import ConvolutionMode
 from deblurring.solvers import Solver
 from evaluation.eval_metrics import calc_dicrepancy_principle, calc_miller_criterion
 
@@ -72,6 +74,7 @@ class Tuner(ABC):
         alphas: list[float],
         L: sp.csr_matrix | Callable[[npt.NDArray, str], sp.csr_matrix],
         save_imgs: bool = False,
+        **kwargs: Any,
     ) -> None:
         """Perform a parameter sweep over a range of regularisation hyper-parameters.
 
@@ -215,11 +218,14 @@ class Tuner(ABC):
 
 
 class StandardTuner(Tuner):
+    """Solver class for tuning regularisation hyper-parameters."""
+
     def parameter_sweep(
         self,
         alphas: list[float],
         L: sp.csr_matrix | Callable[[npt.NDArray, str], sp.csr_matrix],
         save_imgs: bool = False,
+        **kwargs: Any,
     ) -> None:
         """Perform a parameter sweep over a range of regularisation hyper-parameters.
 
@@ -228,6 +234,11 @@ class StandardTuner(Tuner):
             L: Sparse regularisation matrix or function returning it
             save_imgs: Cache deblurred images for each hyper-parameter
         """
+        if not isinstance(L, sp.csr_matrix):
+            raise ValueError(
+                f"L must be of type `scipy.sparse.csr_matrix`, got: {type(L)}",
+            )
+
         self.reset_metrics()
         self._L = L
         self._alphas = []
@@ -249,4 +260,104 @@ class StandardTuner(Tuner):
 
         self.get_optimal_alpha()
         self._optimal_f_hat = self._solver.solve(alpha=self._optimal_alpha, L=self._L)
+        self.calculate_metrics(alpha="optimal", f_hat=self._optimal_f_hat)
+
+
+class IterativeTuner(Tuner):
+    """Iterative solver class for tuning regularisation hyper-parameters."""
+
+    def iteratively_solve(
+        self,
+        alpha: float,
+        L: Callable[[npt.NDArray, str], sp.csr_matrix],
+        prev_f_hat: npt.NDArray,
+        max_iter: int,
+        tol: float,
+    ) -> tuple[npt.NDArray[np.float64], int]:
+        """Iteratively solve the inverse problem.
+
+        Args:
+            alpha: Regularisation parameter
+            L: Regularisation matrix creation function
+            prev_f_hat: Previous deblurred image
+            max_iter: Maximum number of iterations
+            tol: Convergence tolerance
+
+        Returns
+        -------
+            Tuple: Deblurred image and number of iterations
+        """
+        for it in range(max_iter):
+            # Re-initialise regularisation matrix with the last predicted image
+            self._L = L(prev_f_hat, conv_mode=ConvolutionMode.PERIODIC)  # type: ignore[call-arg]
+            f_hat = self._solver.solve(alpha=alpha, L=self._L, x0=prev_f_hat.flatten())
+            prev_f_hat = f_hat
+
+            # Check for convergence
+            residual = self._g.reshape([-1, 1]) - self._kernel(f_hat).reshape([-1, 1])
+            residual_norm = (residual.T @ residual).squeeze()
+
+            if residual_norm <= tol * np.square(self._g).sum():
+                break
+
+        return f_hat, it
+
+    def parameter_sweep(
+        self,
+        alphas: list[float],
+        L: sp.csr_matrix | Callable[[npt.NDArray, str], sp.csr_matrix],
+        save_imgs: bool = False,
+        **kwargs: Any,
+    ) -> None:
+        """Perform a parameter sweep over a range of regularisation hyper-parameters.
+
+        Args:
+            alphas: List of regularisation hyper-parameters
+            L: Sparse regularisation matrix or function returning it
+            save_imgs: Cache deblurred images for each hyper-parameter
+            max_iter: Maximum number of iterations
+            tol: Convergence tolerance
+        """
+        if isinstance(L, sp.csr_matrix):
+            raise ValueError(
+                f"L must be a function returning a sparse matrix, got: {type(L)}",
+            )
+        max_iter: int = kwargs.get("max_iter", 20)
+        tol: float = kwargs.get("tol", 1e-8)
+
+        self.reset_metrics()
+        self._alphas = []
+        self._f_hats = {}
+
+        for alpha in alphas:
+            # Solve for this alpha with blurred image as starting guess
+            self._alphas.append(alpha)
+            f_hat, it = self.iteratively_solve(
+                alpha=alpha,
+                L=L,
+                prev_f_hat=self._g,
+                max_iter=max_iter,
+                tol=tol,
+            )
+
+            # Calculate metrics
+            self.calculate_metrics(alpha=alpha, f_hat=f_hat)
+            if save_imgs:
+                self._f_hats[alpha] = f_hat
+
+            print(
+                f"Alpha {alpha}: DP {self._metrics.loc[alpha, "discrepancy"]}, MSE {self._metrics.loc[alpha, "MSE"]}, Iterations {it + 1}",
+            )
+
+        # Get optimal alpha and iteratively solve
+        self.get_optimal_alpha()
+        self._optimal_f_hat, _ = self.iteratively_solve(
+            self._optimal_alpha,
+            L,
+            self._g,
+            max_iter,
+            tol,
+        )
+
+        # Calculate metrics
         self.calculate_metrics(alpha="optimal", f_hat=self._optimal_f_hat)
