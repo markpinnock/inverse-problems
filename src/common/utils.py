202830b05aea5e37d4e980aab7e5bf1ce94dcc9b
Mark Pinnock <mark.pinnock.18@ucl.ac.uk>
2024-11-17 20:35:10 +0000

    Add toy 1D example

--- src/common/utils.py
+++ src/common/utils.py
@@ -29,3 +29,30 @@ def kernel_to_func(
         return lambda x: kernel @ x.flatten()
 
     return kernel
+
+
+def step_func_gen(N: int) -> tuple[npt.NDArray[np.float64], npt.NDArray[np.float64]]:
+    """Generate toy 1D function.
+
+    Args:
+        N: length
+    """
+    x_min = -1
+    x_max = 1
+    xs = np.linspace(x_min, x_max, N)
+    coords = np.array(
+        [[-0.95, -0.6], [-0.6, -0.2], [-0.2, 0.2], [0.4, 0.6], [0.6, 1.0]],
+    )
+    indices = np.round((coords + 1) / 2 * (N - 1)).astype(np.int16)
+    vals = np.array([1, 0.2, -0.5, 0.7, -0.7])
+    ys = np.zeros(xs.shape)
+
+    for i in range(vals.shape[0]):
+        idx = np.linspace(
+            indices[i][0],
+            indices[i][1],
+            indices[i][1] - indices[i][0] + 1,
+        ).astype(np.int16)
+        ys[idx] = vals[i]
+
+    return xs, ys
