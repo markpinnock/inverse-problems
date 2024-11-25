72cbd52ad4d6e9311c05086c815929674fa58e54
Mark Pinnock <mark.pinnock.18@ucl.ac.uk>
2024-11-25 20:39:24 +0000

    Move one conftest and delete other

--- tests/deblurring/conftest.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import numpy.typing as npt
-import pytest
-
-
-@pytest.fixture  # type: ignore[misc]
-def kernel() -> npt.NDArray[np.float64]:
-    return np.array([[0.0, 0.0, 0.0], [0.0, -1.0, 0.0], [0.0, 0.0, 0.0]])
