4c1ea412dd501b8514054650782a2ebdf5a448b7
Mark Pinnock <mark.pinnock.18@ucl.ac.uk>
2024-12-03 19:11:12 +0000

    Add FFT unit test

--- tests/common/test_fft.py
+++ tests/common/test_fft.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from common.fft import get_min_max, set_up_kspace
+from common.fft import fft_1d, get_min_max, ifft_1d, set_up_kspace, sinc_function
 
 
 @pytest.mark.parametrize("num_samples,expected", [(4, (-2, 1)), (5, (-2, 2))])  # type: ignore[misc]
@@ -27,3 +27,24 @@ def test_set_up_kspace(num_samples: int) -> None:
     k_values_2, x_values_2 = set_up_kspace(k_fov, num_samples)
     assert np.isclose(x_values_1, x_values_2).all()
     assert np.isclose(k_values_1, k_values_2).all()
+
+
+@pytest.mark.parametrize("factor", [1, 2])  # type: ignore[misc]
+def test_fft_1d(factor: int) -> None:
+    """Test both forward and inverse 1D FFT."""
+    # Odd array length case
+    y, x = sinc_function(-10 * factor, 10 * factor, 101)
+    ky, kx = fft_1d(y, x)
+    ry, rx = ifft_1d(ky, kx)
+
+    # Check reconstructed signal and spatial dims are correct
+    assert np.isclose(np.square(ry - y).sum(), 0.0)
+    assert np.isclose(np.square(rx - x).sum(), 0.0)
+
+    # Odd array length case - note that spatial dims are not exact
+    y, x = sinc_function(-10 * factor, 10 * factor, 10)
+    ky, kx = fft_1d(y, x)
+    ry, _ = ifft_1d(ky, kx)
+
+    # Check reconstructed signal is correct
+    assert np.isclose(np.square(ry - y).sum(), 0.0)
