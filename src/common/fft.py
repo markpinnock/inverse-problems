58c4d7131c7f2577398b5bb4e552630553a66777
Mark Pinnock <mark.pinnock.18@ucl.ac.uk>
2024-12-05 19:56:16 +0000

    Add 2D FFT functions

--- src/common/fft.py
+++ src/common/fft.py
@@ -252,3 +252,109 @@ def ifft_1d(
         logger.warning("Result has imaginary component")
 
     return signal.real, x_values
+
+
+def fft_2d(
+    image: npt.NDArray,
+    x_grid: npt.NDArray | None = None,
+    y_grid: npt.NDArray | None = None,
+) -> tuple[
+    npt.NDArray[np.complex128],
+    npt.NDArray[np.float64],
+    npt.NDArray[np.float64],
+]:
+    """Get FFT of 2D image.
+
+    Args:
+        image: pixel intensities at each x_value
+        x_grid: x grid points
+        y_grid: y grid points
+
+    Returns:
+        Tuple: k-space grid values and FFT of image
+    """
+    num_y, num_x = image.shape
+
+    if x_grid is None or y_grid is None:
+        min_x, max_x = get_min_max(num_x)
+        min_y, max_y = get_min_max(num_y)
+        x_grid, y_grid = np.meshgrid(
+            np.linspace(min_x, max_x, num_x),
+            np.linspace(min_y, max_y, num_y),
+        )
+
+    if image.shape != x_grid.shape and image.shape != y_grid.shape:
+        raise ValueError(
+            f"Image and x/y grid must have same shape: {image.shape} vs. {x_grid.shape}  vs. {y_grid.shape}",
+        )
+
+    # Ensure regular grid
+    dx = np.diff(x_grid)
+    dy = np.diff(y_grid)
+    if not np.isclose(dx, dx[0]).all() or not np.isclose(dy, dy[0]).all():
+        raise ValueError("Function only defined for regular spatial coordinates")
+
+    # Generate array of k-space values and FFT
+    kx_values = np.fft.fftshift(np.fft.fftfreq(num_x, x_grid[0, 1] - x_grid[0, 0]))
+    ky_values = np.fft.fftshift(np.fft.fftfreq(num_y, y_grid[1, 0] - y_grid[0, 0]))
+    kx_grid, ky_grid = np.meshgrid(kx_values, ky_values)
+    image_fft = np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(image)))
+
+    if len(ky_values) != image_fft.shape[0] or len(kx_values) != image_fft.shape[1]:
+        raise ValueError(
+            f"K-space and FFT array dims must match: {kx_grid.shape} vs. {ky_grid.shape} vs. {image_fft.shape}",
+        )
+
+    return image_fft, kx_grid, ky_grid
+
+
+def ifft_2d(
+    image_fft: npt.NDArray[np.complex128],
+    kx_grid: npt.NDArray[np.float64],
+    ky_grid: npt.NDArray[np.float64],
+) -> tuple[np.float64, np.float64, np.float64]:
+    """Get inverse FFT of 2D image.
+
+    Args:
+        image_fft: frequency values at each k-value
+        kx_grid: k-space x grid points
+        ky_grid: k-space y grid points
+    """
+    num_y, num_x = image_fft.shape
+
+    if kx_grid is None or ky_grid is None:
+        min_kx, max_kx = get_min_max(num_x)
+        min_ky, max_ky = get_min_max(num_y)
+        kx_grid, ky_grid = np.meshgrid(
+            np.linspace(min_kx, max_kx, num_x),
+            np.linspace(min_ky, max_ky, num_y),
+        )
+
+    if image_fft.shape != kx_grid.shape and image_fft.shape != ky_grid.shape:
+        raise ValueError(
+            f"Image and x/y grid must have same shape: {image_fft.shape} vs. {kx_grid.shape}  vs. {ky_grid.shape}",
+        )
+
+    # Ensure regular grid
+    dkx = np.diff(kx_grid)
+    dky = np.diff(ky_grid)
+    if not np.isclose(dkx, dkx[0]).all() or not np.isclose(dky, dky[0]).all():
+        raise ValueError(
+            "Function only defined for regular spatial frequency coordinates",
+        )
+
+    # Generate array of spatial grid values and original image
+    x_values = np.fft.fftshift(np.fft.fftfreq(num_x, kx_grid[0, 1] - kx_grid[0, 0]))
+    y_values = np.fft.fftshift(np.fft.fftfreq(num_y, ky_grid[1, 0] - ky_grid[0, 0]))
+    x_grid, y_grid = np.meshgrid(x_values, y_values)
+    image = np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(image_fft)))
+
+    if len(y_values) != image.shape[0] or len(x_values) != image.shape[1]:
+        raise ValueError(
+            f"Spatial and image array dims must match: {x_grid.shape} vs. {y_grid.shape} vs. {image.shape}",
+        )
+
+    if not np.isclose(image.imag.sum(), 0.0):
+        logger.warning("Result has imaginary component")
+
+    return image.real, x_grid, y_grid
