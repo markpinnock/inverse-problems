8caf051769bb590b0bc569ffc3f10e5733ab3e05
Mark Pinnock <mark.pinnock.18@ucl.ac.uk>
2024-12-05 19:56:42 +0000

    Add 2D FFT unit tests

--- tests/common/test_fft.py
+++ tests/common/test_fft.py
@@ -1,7 +1,16 @@
 import numpy as np
 import pytest
 
-from common.fft import fft_1d, get_min_max, ifft_1d, set_up_kspace, sinc_function
+from common.fft import (
+    fft_1d,
+    fft_2d,
+    get_min_max,
+    ifft_1d,
+    ifft_2d,
+    set_up_kspace,
+    sinc_function_1d,
+    sinc_function_2d,
+)
 
 
 @pytest.mark.parametrize("num_samples,expected", [(4, (-2, 1)), (5, (-2, 2))])  # type: ignore[misc]
@@ -33,7 +42,7 @@ def test_set_up_kspace(num_samples: int) -> None:
 def test_fft_1d(factor: int) -> None:
     """Test both forward and inverse 1D FFT."""
     # Odd array length case
-    y, x = sinc_function(-10 * factor, 10 * factor, 101)
+    y, x = sinc_function_1d(x_range=(-10 * factor, 10 * factor), num_samples=101)
     ky, kx = fft_1d(y, x)
     ry, rx = ifft_1d(ky, kx)
 
@@ -42,9 +51,41 @@ def test_fft_1d(factor: int) -> None:
     assert np.isclose(np.square(rx - x).sum(), 0.0)
 
     # Odd array length case - note that spatial dims are not exact
-    y, x = sinc_function(-10 * factor, 10 * factor, 10)
+    y, x = sinc_function_1d(x_range=(-10 * factor, 10 * factor), num_samples=10)
     ky, kx = fft_1d(y, x)
     ry, _ = ifft_1d(ky, kx)
 
     # Check reconstructed signal is correct
     assert np.isclose(np.square(ry - y).sum(), 0.0)
+
+
+@pytest.mark.parametrize("factor", [1, 2])  # type: ignore[misc]
+def test_fft_2d(factor: int) -> None:
+    """Test both forward and inverse 2D FFT."""
+    # Odd array length case
+    z, x, y = sinc_function_2d(
+        x_range=(-10 * factor, 10 * factor),
+        y_range=(-20, 20),
+        num_x=101,
+        num_y=101,
+    )
+    kz, kx, ky = fft_2d(z, x, y)
+    rz, rx, ry = ifft_2d(kz, kx, ky)
+
+    # Check reconstructed signal and spatial dims are correct
+    assert np.isclose(np.square(rz - z).sum(), 0.0)
+    assert np.isclose(np.square(rx - x).sum(), 0.0)
+    assert np.isclose(np.square(ry - y).sum(), 0.0)
+
+    # Odd array length case - note that spatial dims are not exact
+    z, x, y = sinc_function_2d(
+        x_range=(-10 * factor, 10 * factor),
+        y_range=(-20, 20),
+        num_x=100,
+        num_y=100,
+    )
+    kz, kx, ky = fft_2d(z, x, y)
+    rz, _, _ = ifft_2d(kz, kx, ky)
+
+    # Check reconstructed signal is correct
+    assert np.isclose(np.square(rz - z).sum(), 0.0)
